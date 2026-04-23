a5c86abb692caee2814ad88cbd215510991b66ad
chadhyer <chadhyer92@gmail.com>
2026-04-23 15:50:23 +0100

    moved docker files into Docker directory, removed unused ansible.cfg, and created logic in run.sh to build docker image if missing

--- run.sh
+++ run.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
+image=proxmox-kube-deployer
+build=FALSE
 
 # Arguments
 while (( "$#" )); do
     case "$1" in
-        -i|--image) image=$2;shift;;
         -t|--tag) tag=$2;shift;;
         -n|--name) name=$2;shift;;
         -e|--env) env=$2;shift;;
         -u|--uid) uid=$2;shift;;
         -g|--gid) gid=$2;shift;;
+        -b|--build) build=TRUE;;
     esac
     shift
 done
 
+# Check if image exists
+if [ $(docker images --format 'table'|grep -c ${image}) -lt 1 ];then
+    read -p "$image image is missing. Would you like to build? <Y|n>" bld
+    case $bld in
+        [nN]* ) echo 'Exiting script...';exit 0;;
+        *) echo 'Building image...';build=TRUE;;
+    esac
+fi
+if [ "${build}" == "TRUE" ];then
+    cd ./Docker/
+    docker build -t ${image}:v$(date -I) .
+    if [ $? == 1 ];then
+        echo 'Something went wrong with docker build command! Exiting...'
+        exit 1
+    fi
+    cd ../
+fi
+
 # Default values
-if [ -z "${image}" ];then image="${IMAGE:-proxmox-kube-deployer}";fi
 if [ -z "${tag}" ];then
     tag="$(docker images --format table|grep $image|head -n 1|awk '{print $2}')"
     echo "No tag provided and found: '${tag}'"
